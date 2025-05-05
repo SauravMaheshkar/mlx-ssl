e911e8b32de3f8a0acaaba288867ec1a178a49a1
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-05-05 03:26:06 +0100

    feat(data2vec): add VisionSelfAttention

--- tests/models/data2vec/test_layers.py
+++ tests/models/data2vec/test_layers.py
@@ -1,7 +1,7 @@
 import mlx.core as mx
 import pytest
 
-from src.models.data2vec.layers import PatchEncoder, VisionEmbeddings
+from src.models.data2vec.layers import PatchEncoder, VisionEmbeddings, VisionSelfAttention
 
 
 @pytest.mark.parametrize(
@@ -50,3 +50,72 @@ def test_vision_embeddings(
     assert embeddings.shape == (1, seq_len + 1, hidden_size)
     assert height == patch_height
     assert width == patch_width
+
+
+@pytest.mark.parametrize(
+    "hidden_size, num_attention_heads, seq_len, batch_size , image_size, num_channels, patch_size",  # noqa: E501
+    [
+        (768, 12, 196, 1, 224, 3, 16),
+        (512, 8, 64, 1, 256, 3, 32),
+    ],
+)
+def test_vision_self_attention(
+    hidden_size,
+    num_attention_heads,
+    seq_len,
+    batch_size,
+    image_size,
+    num_channels,
+    patch_size,
+):
+    attn_module = VisionSelfAttention(
+        hidden_size=hidden_size,
+        num_attention_heads=num_attention_heads,
+    )
+    encoder = PatchEncoder(
+        image_size=image_size,
+        patch_size=patch_size,
+        num_channels=num_channels,
+        hidden_size=hidden_size,
+    )
+
+    pixel_values = mx.ones((batch_size, image_size, image_size, num_channels))
+    hidden_states, _ = encoder(pixel_values)
+
+    context_layer = attn_module(hidden_states)
+    assert context_layer.shape == (batch_size, seq_len, hidden_size)
+
+
+@pytest.mark.parametrize(
+    "hidden_size, num_attention_heads, seq_len, batch_size , image_size, num_channels, patch_size",  # noqa: E501
+    [
+        (768, 12, 196, 1, 224, 3, 16),
+        (512, 8, 64, 1, 256, 3, 32),
+    ],
+)
+def test_vision_self_attention_with_attn_output(
+    hidden_size,
+    num_attention_heads,
+    seq_len,
+    batch_size,
+    image_size,
+    num_channels,
+    patch_size,
+):
+    attn_module = VisionSelfAttention(
+        hidden_size=hidden_size,
+        num_attention_heads=num_attention_heads,
+    )
+    encoder = PatchEncoder(
+        image_size=image_size,
+        patch_size=patch_size,
+        num_channels=num_channels,
+        hidden_size=hidden_size,
+    )
+
+    pixel_values = mx.ones((batch_size, image_size, image_size, num_channels))
+    hidden_states, _ = encoder(pixel_values)
+
+    context_layer, attn_probs = attn_module(hidden_states, output_attns=True)
+    assert context_layer.shape == (batch_size, seq_len, hidden_size)
+    assert attn_probs.shape == (batch_size, num_attention_heads, seq_len, seq_len)
