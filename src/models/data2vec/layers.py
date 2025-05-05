a0a0178ec93466c9d28d413ca22ec02bbba855fc
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-05-05 03:06:35 +0100

    feat(data2vec): add VisionEmbeddings

--- src/models/data2vec/layers.py
+++ src/models/data2vec/layers.py
@@ -31,10 +31,6 @@ class PatchEncoder(nn.Module):
         self.num_patches = num_patches
         self.patch_shape = patch_shape
 
-        print(
-            f"in_channels: {num_channels}, out_channels: {hidden_size}, kernel_size: {patch_size}, stride: {patch_size}"  # noqa: E501
-        )
-
         self.projection = nn.Conv2d(
             in_channels=num_channels,
             out_channels=hidden_size,
@@ -43,6 +39,15 @@ class PatchEncoder(nn.Module):
         )
 
     def __call__(self, pixel_values: mx.array) -> Tuple[mx.array, Tuple[int, int]]:
+        """
+
+        Args:
+            pixel_values: (batch_size, height, width, num_channels)
+
+        Returns:
+            embeddings: (batch_size, seq_len, hidden_size)
+            (patch_height, patch_width): the height and width of the patches
+        """
         num_channels = pixel_values.shape[-1]
 
         if num_channels != self.num_channels:
@@ -53,5 +58,54 @@ class PatchEncoder(nn.Module):
         embeddings = self.projection(pixel_values)
         patch_height, patch_width = embeddings.shape[1], embeddings.shape[2]
         embeddings = embeddings.flatten(1, 2)
+
+        return embeddings, (patch_height, patch_width)
+
+
+class VisionEmbeddings(nn.Module):
+    def __init__(
+        self,
+        image_size: int,
+        patch_size: int,
+        num_channels: int,
+        hidden_size: int,
+        hidden_dropout_prob: float = 0.0,
+    ):
+        super().__init__()
+
+        self.cls_token = mx.zeros((1, 1, hidden_size))
+        self.mask_token = None
+        self.patch_encoder = PatchEncoder(
+            image_size=image_size,
+            patch_size=patch_size,
+            num_channels=num_channels,
+            hidden_size=hidden_size,
+        )
+        self.patch_size = patch_size
+        self.image_size = (
+            image_size
+            if isinstance(image_size, collections.abc.Iterable)
+            else (image_size, image_size)
+        )
+        self.position_embeddings = None
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+
+    def __call__(self, pixel_values: mx.array) -> Tuple[mx.array, Tuple[int, int]]:
+        """
+
+        Args:
+            pixel_values: (batch_size, height, width, num_channels)
+
+        Returns:
+            embeddings: (batch_size, seq_len + 1, hidden_size)
+            (patch_height, patch_width): the height and width of the patches
+        """
+        embeddings, (patch_height, patch_width) = self.patch_encoder(pixel_values)
+        batch_size, _, _ = embeddings.shape
+
+        cls_token = mx.repeat(self.cls_token, repeats=batch_size, axis=0)
+        embeddings = mx.concatenate([cls_token, embeddings], axis=1)
+
+        embeddings = self.dropout(embeddings)
 
         return embeddings, (patch_height, patch_width)
--- tests/models/data2vec/test_layers.py
+++ tests/models/data2vec/test_layers.py
@@ -1,13 +1,52 @@
 import mlx.core as mx
+import pytest
 
-from src.models.data2vec.layers import PatchEncoder
+from src.models.data2vec.layers import PatchEncoder, VisionEmbeddings
 
 
-def test_patch_encoder():
-    encoder = PatchEncoder(image_size=224, patch_size=16, num_channels=3, hidden_size=768)
+@pytest.mark.parametrize(
+    "image_size, patch_size, num_channels, hidden_size, seq_len, patch_height, patch_width",  # noqa: E501
+    [
+        (224, 16, 3, 768, 196, 14, 14),
+        (256, 32, 3, 512, 64, 8, 8),
+    ],
+)
+def test_patch_encoder(
+    image_size, patch_size, num_channels, hidden_size, seq_len, patch_height, patch_width
+):
+    encoder = PatchEncoder(
+        image_size=image_size,
+        patch_size=patch_size,
+        num_channels=num_channels,
+        hidden_size=hidden_size,
+    )
 
-    pixel_values = mx.ones((1, 224, 224, 3))
-    embeddings, (patch_height, patch_width) = encoder(pixel_values)
-    assert embeddings.shape == (1, 196, 768)
-    assert patch_height == 14
-    assert patch_width == 14
+    pixel_values = mx.ones((1, image_size, image_size, num_channels))
+    embeddings, (height, width) = encoder(pixel_values)
+    assert embeddings.shape == (1, seq_len, hidden_size)
+    assert height == patch_height
+    assert width == patch_width
+
+
+@pytest.mark.parametrize(
+    "image_size, patch_size, num_channels, hidden_size, seq_len, patch_height, patch_width",  # noqa: E501
+    [
+        (224, 16, 3, 768, 196, 14, 14),
+        (256, 32, 3, 512, 64, 8, 8),
+    ],
+)
+def test_vision_embeddings(
+    image_size, patch_size, num_channels, hidden_size, seq_len, patch_height, patch_width
+):
+    module = VisionEmbeddings(
+        image_size=image_size,
+        patch_size=patch_size,
+        num_channels=num_channels,
+        hidden_size=hidden_size,
+    )
+
+    pixel_values = mx.ones((1, image_size, image_size, num_channels))
+    embeddings, (height, width) = module(pixel_values)
+    assert embeddings.shape == (1, seq_len + 1, hidden_size)
+    assert height == patch_height
+    assert width == patch_width
